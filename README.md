496ed97b8d10da4a87e3b33f2abb41a9f010066e
Robert Schaefer <robert@hiddentrends.com>
2015-04-26 15:51:26 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Course Project - Getting and Cleaning Data
+
+This repo contains the follwing 3 files:
+
+- README.md
+- Codebook.md - the codebook for this project
+- run_analysis.R - the R script that does the tidying
+
+#Requirements
+
+run_analysis.R requires the following packages:
+
+- reshape2
+
+Furthermore, run_analysis.R expect the working directory to have the Samsung data sets residing in the "UCI HAR Dataset/" directory.
+
+The output of the script is a tidy data set called tinydata.txt
---
+++ run_analysis.R
@@ -0,0 +1,74 @@
+#Course Project
+
+#this file contains the code for the course project.  It takes multiple
+#files and combines them into a single data frame. 
+
+#read activity labels
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt", header=FALSE)
+
+#read test measurements
+testResults <- read.table("UCI HAR Dataset/test/X_test.txt", header=FALSE)
+
+#read test subjects
+testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = c("Subject"), header=FALSE)
+
+#read test activity labels
+testActivity <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = c("Activity"), header=FALSE)
+
+#read training measurements
+trainResults <- read.table("UCI HAR Dataset/train/X_train.txt", header=FALSE)
+
+#read training activity labels
+trainActivity <- read.table("UCI HAR Dataset/train/y_train.txt", col.names=c("Activity"),header=FALSE)
+
+#read train subjects
+trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = c("Subject"), header=FALSE)
+
+#read variable names
+variables <- read.table("UCI HAR Dataset/features.txt", header=FALSE)
+
+#use gsub to make variable names more descriptive
+variables$V2 <- gsub("\\(\\)","", variables$V2)
+variables$V2 <- gsub("tBody","TimeBody",variables$V2)
+variables$V2 <- gsub("tGravity","TimeGravity",variables$V2)
+variables$V2 <- gsub("fBody","FrequencyBody",variables$V2)
+variables$V2 <- gsub("fGravity","FrequencyGravity",variables$V2)
+variables$V2 <- gsub("-mean-","Mean",variables$V2)
+variables$V2 <- gsub("-std-","StdDev",variables$V2)
+variables$V2 <- gsub("Acc", "Acceleration", variables$V2)
+variables$V2 <- gsub("-mean","Mean",variables$V2)
+variables$V2 <- gsub("-std","StdDev",variables$V2)
+
+#make column names = variables in features.txt
+colnames(trainResults) = variables$V2
+colnames(testResults) = variables$V2
+
+#create complete test data frame
+test <- cbind(testSubjects,testActivity,testResults)
+
+#create complete train data frame
+train <- cbind(trainSubjects,trainActivity,trainResults)
+
+#create merge test and train
+fullDataset <- rbind(test,train)
+
+#subset to only columns with MEAN and STD
+filtered <- fullDataset[,c("Subject","Activity", colnames(fullDataset)[grep("mean",colnames(fullDataset))], colnames(fullDataset)[grep("std",colnames(fullDataset))])]
+
+#replace activity ID with label
+activityList <- as.vector(activityLabels$V2)
+labels <- activityList[filtered$Activity]
+filtered$Activity <- labels
+
+#call reshape library
+library(reshape)
+
+#melt into narrow format (subject,activity, variable, value)
+narrow <- melt(filtered, id=c("Subject","Activity"))
+
+#now aggregate and get avg for each subject-activity-variable combo
+wide <- dcast(narrow, Subject + Activity ~ variable, fun.aggregate = mean)
+
+#write tidy data set
+write.table(wide, file="tidydata.txt", row.names = FALSE)
+
