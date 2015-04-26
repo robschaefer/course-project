3d991d13e25a5b96936d947fbdd95a4da5fa4161
Robert Schaefer <robert@hiddentrends.com>
2015-04-26 16:35:08 -0400

    update R script

--- run_analysis.R
+++ run_analysis.R
@@ -27,17 +27,7 @@ trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names
 #read variable names
 variables <- read.table("UCI HAR Dataset/features.txt", header=FALSE)
 
-#use gsub to make variable names more descriptive
-variables$V2 <- gsub("\\(\\)","", variables$V2)
-variables$V2 <- gsub("tBody","TimeBody",variables$V2)
-variables$V2 <- gsub("tGravity","TimeGravity",variables$V2)
-variables$V2 <- gsub("fBody","FrequencyBody",variables$V2)
-variables$V2 <- gsub("fGravity","FrequencyGravity",variables$V2)
-variables$V2 <- gsub("-mean-","Mean",variables$V2)
-variables$V2 <- gsub("-std-","StdDev",variables$V2)
-variables$V2 <- gsub("Acc", "Acceleration", variables$V2)
-variables$V2 <- gsub("-mean","Mean",variables$V2)
-variables$V2 <- gsub("-std","StdDev",variables$V2)
+
 
 #make column names = variables in features.txt
 colnames(trainResults) = variables$V2
@@ -68,6 +58,22 @@ narrow <- melt(filtered, id=c("Subject","Activity"))
 
 #now aggregate and get avg for each subject-activity-variable combo
 wide <- dcast(narrow, Subject + Activity ~ variable, fun.aggregate = mean)
+
+#use gsub to make variable names more descriptive
+
+colnames(wide) <- gsub("\\(\\)","", colnames(wide))
+colnames(wide) <- gsub("tBody","TimeBody",colnames(wide))
+colnames(wide) <- gsub("tGravity","TimeGravity",colnames(wide))
+colnames(wide) <- gsub("fBody","FrequencyBody",colnames(wide))
+colnames(wide) <- gsub("fGravity","FrequencyGravity",colnames(wide))
+colnames(wide) <- gsub("-mean-","Mean",colnames(wide))
+colnames(wide) <- gsub("-std-","StdDev",colnames(wide))
+colnames(wide) <- gsub("Acc", "Acceleration", colnames(wide))
+colnames(wide) <- gsub("-mean","Mean",colnames(wide))
+colnames(wide) <- gsub("-std","StdDev",colnames(wide))
+colnames(wide) <- gsub("Freq-X","FreqX",colnames(wide))
+colnames(wide) <- gsub("Freq-Y","FreqY",colnames(wide))
+colnames(wide) <- gsub("Freq-Z","FreqZ",colnames(wide))
 
 #write tidy data set
 write.table(wide, file="tidydata.txt", row.names = FALSE)
